90a47f1a96bdb1cdbe363e773496457d49094018
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2025-02-24 17:11:44 +0700

    update library

--- frontend_ecommerce_interior/src/Custom/Router/RouterCustom.jsx
+++ frontend_ecommerce_interior/src/Custom/Router/RouterCustom.jsx
@@ -2,12 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Register from "../../components/Auth/Register/Register";
 import Login from "../../components/Auth/Login/Login";
 import Verify from "../../components/Modals/verify";
+import Home from "../../page/home/Home";
 
 const RouterCustom = () => {
   return (
     <Routes>
-      <Route path="/" element={<>vox chis thanh </>} />
-
+      <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/verify/:id" element={<Verify />} />
---
+++ frontend_ecommerce_interior/src/page/home/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div className="text-red-200 bg-green-300">Trang chu ne nha</div>;
+};
+
+export default Home;
