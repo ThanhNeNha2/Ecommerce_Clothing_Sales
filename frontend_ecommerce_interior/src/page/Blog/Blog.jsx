0d60dfcaf5dd7fde016deb0518c60bc4864b5c63
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2025-03-02 19:11:28 +0700

    FE page blog 2

--- frontend_ecommerce_interior/src/page/Blog/Blog.jsx
+++ frontend_ecommerce_interior/src/page/Blog/Blog.jsx
@@ -6,6 +6,8 @@ import { FaAngleRight, FaTag, FaUser } from "react-icons/fa";
 import { BsCalendar2DateFill } from "react-icons/bs";
 import { IoSearchSharp } from "react-icons/io5";
 import CoverImg from "../../components/Cover/CoverImg";
+import Quality from "../../components/Quality/Quality";
+import Footer from "../../components/Footer/Footer";
 
 const Blog = () => {
   return (
@@ -14,20 +16,20 @@ const Blog = () => {
       {/* slider */}
       <CoverImg namePage="Blog" />
       {/*  */}
-      <div className="  px-[120px] pt-16 flex gap-10">
-        <div className="flex-[5]   flex justify-center flex-col gap-20 ">
+      <div className="  px-[200px] pt-16 flex gap-10">
+        <div className="flex-[4]   flex justify-center flex-col gap-20 ">
           {/* bai 1  */}
-          <div className="flex  flex-col gap-3  w-[817px]">
+          <div className="flex  flex-col gap-3  w-full">
             {/* anh */}
             <div className="flex justify-center bg-red-50 w-full rounded">
               <img
                 src="https://images.pexels.com/photos/1422286/pexels-photo-1422286.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                 alt=""
-                className=" w-full h-[500px] rounded"
+                className=" w-full h-[400px] rounded"
               />
             </div>
             {/*  */}
-            <div className="flex gap-5 ">
+            <div className="flex gap-5 text-sm">
               <div className="flex items-center gap-1 text-gray-500">
                 <FaUser /> Admin
               </div>
@@ -38,10 +40,10 @@ const Blog = () => {
                 <FaTag /> Wood
               </div>
             </div>
-            <h1 className="font-poppins font-semibold text-2xl">
+            <h1 className="font-poppins font-semibold text-xl">
               Going all-in with millennial design
             </h1>
-            <span>
+            <span className="text-sm text-gray-400">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus
               mauris vitae ultricies leo integer malesuada nunc. In nulla
@@ -59,17 +61,17 @@ const Blog = () => {
             </div>
           </div>
           {/* bai 1  */}
-          <div className="flex  flex-col gap-3  w-[817px]">
+          <div className="flex  flex-col gap-3  w-full">
             {/* anh */}
             <div className="flex justify-center bg-red-50 w-full rounded">
               <img
                 src="https://images.pexels.com/photos/1422286/pexels-photo-1422286.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                 alt=""
-                className=" w-full h-[500px] rounded"
+                className=" w-full h-[400px] rounded"
               />
             </div>
             {/*  */}
-            <div className="flex gap-5 ">
+            <div className="flex gap-5 text-sm">
               <div className="flex items-center gap-1 text-gray-500">
                 <FaUser /> Admin
               </div>
@@ -80,10 +82,10 @@ const Blog = () => {
                 <FaTag /> Wood
               </div>
             </div>
-            <h1 className="font-poppins font-semibold text-2xl">
+            <h1 className="font-poppins font-semibold text-xl">
               Going all-in with millennial design
             </h1>
-            <span>
+            <span className="text-sm text-gray-400">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus
               mauris vitae ultricies leo integer malesuada nunc. In nulla
@@ -99,19 +101,19 @@ const Blog = () => {
                 Read more
               </span>
             </div>
-          </div>{" "}
+          </div>
           {/* bai 1  */}
-          <div className="flex  flex-col gap-3  w-[817px]">
+          <div className="flex  flex-col gap-3  w-full">
             {/* anh */}
             <div className="flex justify-center bg-red-50 w-full rounded">
               <img
                 src="https://images.pexels.com/photos/1422286/pexels-photo-1422286.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                 alt=""
-                className=" w-full h-[500px] rounded"
+                className=" w-full h-[400px] rounded"
               />
             </div>
             {/*  */}
-            <div className="flex gap-5 ">
+            <div className="flex gap-5 text-sm">
               <div className="flex items-center gap-1 text-gray-500">
                 <FaUser /> Admin
               </div>
@@ -122,10 +124,10 @@ const Blog = () => {
                 <FaTag /> Wood
               </div>
             </div>
-            <h1 className="font-poppins font-semibold text-2xl">
+            <h1 className="font-poppins font-semibold text-xl">
               Going all-in with millennial design
             </h1>
-            <span>
+            <span className="text-sm text-gray-400">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus
               mauris vitae ultricies leo integer malesuada nunc. In nulla
@@ -141,19 +143,19 @@ const Blog = () => {
                 Read more
               </span>
             </div>
-          </div>{" "}
+          </div>
           {/* bai 1  */}
-          <div className="flex  flex-col gap-3  w-[817px]">
+          <div className="flex  flex-col gap-3  w-full">
             {/* anh */}
             <div className="flex justify-center bg-red-50 w-full rounded">
               <img
                 src="https://images.pexels.com/photos/1422286/pexels-photo-1422286.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                 alt=""
-                className=" w-full h-[500px] rounded"
+                className=" w-full h-[400px] rounded"
               />
             </div>
             {/*  */}
-            <div className="flex gap-5 ">
+            <div className="flex gap-5 text-sm">
               <div className="flex items-center gap-1 text-gray-500">
                 <FaUser /> Admin
               </div>
@@ -164,10 +166,10 @@ const Blog = () => {
                 <FaTag /> Wood
               </div>
             </div>
-            <h1 className="font-poppins font-semibold text-2xl">
+            <h1 className="font-poppins font-semibold text-xl">
               Going all-in with millennial design
             </h1>
-            <span>
+            <span className="text-sm text-gray-400">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus
               mauris vitae ultricies leo integer malesuada nunc. In nulla
@@ -185,7 +187,7 @@ const Blog = () => {
             </div>
           </div>
         </div>
-        <div className="flex-[2] bg-gray-200">
+        <div className="flex-[2] flex flex-col gap-5 ">
           <div className=" ">
             <div className="relative w-[70%]">
               <input type="text" className="w-full border py-1 rounded pr-8" />
@@ -194,10 +196,104 @@ const Blog = () => {
               </div>
             </div>
           </div>
-          <div></div>
-          <div></div>
+          {/* Categories */}
+          <div className=" flex flex-col gap-3">
+            <span className="font-poppins font-medium text-xl">Categories</span>
+            <ul className="w-[80%] px-5 flex flex-col gap-5">
+              <li className="flex justify-between text-gray-400">
+                <span>Crafts</span>
+                <p>2</p>
+              </li>
+              <li className="flex justify-between text-gray-400">
+                <span>Design</span>
+                <p>8</p>
+              </li>{" "}
+              <li className="flex justify-between text-gray-400">
+                <span>Handmade</span>
+                <p>7</p>
+              </li>{" "}
+              <li className="flex justify-between text-gray-400">
+                <span>Interior</span>
+                <p>2</p>
+              </li>
+              <li className="flex justify-between text-gray-400">
+                <span>Wood</span>
+                <p>6</p>
+              </li>
+            </ul>
+          </div>
+          {/*   Recent Posts */}
+          <div className=" flex flex-col gap-3 mt-16">
+            <span className="font-poppins font-medium text-xl">
+              Recent Posts
+            </span>
+            <ul className="w-[100%]  flex flex-col gap-5">
+              <li className="flex gap-5 text-gray-400">
+                <div className="w-[100px] h-[100px] ">
+                  <img
+                    src="https://images.pexels.com/photos/8581013/pexels-photo-8581013.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    alt=""
+                    className="h-full w-full object-cover  object-bottom"
+                  />
+                </div>
+                <div className="p-1">
+                  <span className="text-black ">
+                    Going all-in with millennial design
+                  </span>
+                  <p className="text-sm">03 Aug 2022</p>
+                </div>
+              </li>
+              <li className="flex gap-5 text-gray-400">
+                <div className="w-[100px] h-[100px] ">
+                  <img
+                    src="https://images.pexels.com/photos/8581013/pexels-photo-8581013.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    alt=""
+                    className="h-full w-full object-cover  object-bottom"
+                  />
+                </div>
+                <div className="p-1">
+                  <span className="text-black ">
+                    Going all-in with millennial design
+                  </span>
+                  <p className="text-sm">03 Aug 2022</p>
+                </div>
+              </li>{" "}
+              <li className="flex gap-5 text-gray-400">
+                <div className="w-[100px] h-[100px] ">
+                  <img
+                    src="https://images.pexels.com/photos/8581013/pexels-photo-8581013.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    alt=""
+                    className="h-full w-full object-cover  object-bottom"
+                  />
+                </div>
+                <div className="p-1">
+                  <span className="text-black ">
+                    Going all-in with millennial design
+                  </span>
+                  <p className="text-sm">03 Aug 2022</p>
+                </div>
+              </li>{" "}
+              <li className="flex gap-5 text-gray-400">
+                <div className="w-[100px] h-[100px] ">
+                  <img
+                    src="https://images.pexels.com/photos/8581013/pexels-photo-8581013.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    alt=""
+                    className="h-full w-full object-cover  object-bottom"
+                  />
+                </div>
+                <div className="p-1">
+                  <span className="text-black ">
+                    Going all-in with millennial design
+                  </span>
+                  <p className="text-sm">03 Aug 2022</p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
+      <Quality />
+      <Footer />
     </div>
   );
 };
