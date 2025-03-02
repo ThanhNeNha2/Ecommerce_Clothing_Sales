9c531f40dbb9e6aad2fbfea99e1c1e9bb8b73b78
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2025-03-02 16:31:43 +0700

    FE page compare 2

--- frontend_ecommerce_interior/src/page/ProductComparison/ProductComparison.jsx
+++ frontend_ecommerce_interior/src/page/ProductComparison/ProductComparison.jsx
@@ -101,7 +101,6 @@ const ProductComparison = () => {
                 Asgaard sofa
               </span>
               <p className="font-poppins text-[15px] text-gray-400">
-                {" "}
                 Rs. 250,000.00
               </p>
               <div className="flex items-center gap-3">
@@ -133,7 +132,6 @@ const ProductComparison = () => {
                 Asgaard sofa
               </span>
               <p className="font-poppins text-[15px] text-gray-400">
-                {" "}
                 Rs. 250,000.00
               </p>
               <div className="flex items-center gap-3">
@@ -165,6 +163,445 @@ const ProductComparison = () => {
                 </option>
               ))}
             </select>
+          </div>
+        </div>
+        {/* Chi tiet san pham  */}
+        <div className="flex flex-col">
+          {/* 1 */}
+          <div className="flex border-t ">
+            <div className="  w-[calc(100%/4)] px-7 pt-7 pb-4  border-r  ">
+              <span className="font-poppins font-medium text-lg">General</span>
+            </div>
+            <div className="  w-[calc(100%/4)] p-7  border-r">
+              <span className="hidden">General</span>
+            </div>
+            <div className="  w-[calc(100%/4)] p-7   border-r">
+              <span className="hidden">General</span>
+            </div>
+            <div className="  w-[calc(100%/4)] p-7  hidden">
+              <span>General</span>
+            </div>
+          </div>
+          {/* 2 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Sales Package</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>1 sectional sofa</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>1 Three Seater, 2 Single Seater</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+          {/*  */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Secondary Material</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>Solid Wood</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>Solid Wood</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+          {/* 3 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Configuration</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>TFCBLIGRBL6SRHS</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>DTUBLIGRBL568</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+          {/* 4 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Configuration</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>L-shaped</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>L-shaped</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+
+          {/* 5 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Upholstery Material</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>Fabric + Cotton</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>Fabric + Cotton</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+
+          {/* 6 */}
+          <div className="flex ">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span className="   m-auto">Upholstery Color</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>Bright Grey & Lion</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>Bright Grey & Lion</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+
+          {/* ****  VER 2 **** */}
+
+          {/* 1 */}
+          <div className="flex  ">
+            <div className="  w-[calc(100%/4)] px-7 pt-16 pb-4 border-r  ">
+              <span className="font-poppins font-medium text-xl">Product</span>
+            </div>
+            <div className="  w-[calc(100%/4)] p-7  border-r">
+              <span className="hidden">General</span>
+            </div>
+            <div className="  w-[calc(100%/4)] p-7   border-r">
+              <span className="hidden">General</span>
+            </div>
+            <div className="  w-[calc(100%/4)] p-7  hidden">
+              <span>General</span>
+            </div>
+          </div>
+          {/* 2 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Filling Material</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>Foam</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>Matter</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+          {/* 3 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Finish Type</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>Bright Grey & Lion</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>Bright Grey & Lion</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+          {/* 4 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Adjustable Headrest</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>No</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>Yes</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+          {/* 5 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Maximum Load Capacity</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>280 KG</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>280 KG</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+          {/* 6 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Origin of Manufacture</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>India</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>India</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+
+          {/* ****  VER 3 **** */}
+
+          {/* 1 */}
+          <div className="flex  ">
+            <div className="  w-[calc(100%/4)] px-7 pt-16 pb-4 border-r  ">
+              <span className="font-poppins font-medium text-xl">
+                Dimensions
+              </span>
+            </div>
+            <div className="  w-[calc(100%/4)] p-7  border-r">
+              <span className="hidden">General</span>
+            </div>
+            <div className="  w-[calc(100%/4)] p-7   border-r">
+              <span className="hidden">General</span>
+            </div>
+            <div className="  w-[calc(100%/4)] p-7  hidden">
+              <span>General</span>
+            </div>
+          </div>
+          {/* 2 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Width</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>265.32 cm</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>265.32 cm</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+          {/* 3 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Hight</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>65 cm</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>65 cm</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+          {/* 4 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Depth</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>167.76 cm</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>167.76 cm</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+          {/* 5 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Weight</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>65 KG</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>65 KG</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+          {/* 6 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Seat Height</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>41.52 cm</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>41.52 cm</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+          {/* 7 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Leg Height</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>5.46 cm</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>5.46 cm</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+
+          {/* ****  VER 3 **** */}
+
+          {/* 1 */}
+          <div className="flex  ">
+            <div className="  w-[calc(100%/4)] px-7 pt-16 pb-4 border-r  ">
+              <span className="font-poppins font-medium text-xl">Warranty</span>
+            </div>
+            <div className="  w-[calc(100%/4)] p-7  border-r">
+              <span className="hidden">General</span>
+            </div>
+            <div className="  w-[calc(100%/4)] p-7   border-r">
+              <span className="hidden">General</span>
+            </div>
+            <div className="  w-[calc(100%/4)] p-7  hidden">
+              <span>General</span>
+            </div>
+          </div>
+          {/* 2 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Warranty Summary</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>1 Year Manufacturing Warranty</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>1 Year Manufacturing Warranty</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+          {/* 3 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Warranty Service Type</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>For Warranty Claims or Any Product Related </span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>
+                For Warranty Claims or Any Product Related Issues Please Email
+                at support@xyz.com
+              </span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+          {/* 4 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Covered in Warranty</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>Warranty Against Manufacturing Defect</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>
+                Warranty of the product is limited to manufacturing defects
+                only.
+              </span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+          {/* 5 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Not Covered in Warranty</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>
+                The Warranty Does Not Cover Damages Due To Usage Of The Product
+                Beyond Its Intended Use And Wear & Tear In The Natural Course Of
+                Product Usage.
+              </span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>
+                The Warranty Does Not Cover Damages Due To Usage Of The Product
+                Beyond Its Intended Use And Wear & Tear In The Natural Course Of
+                Product Usage.
+              </span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+          {/* 6 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span>Domestic Warranty</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r">
+              <span>1 Year</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r ">
+              <span>3 Months</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span>...</span>
+            </div>
+          </div>
+          {/* 6 */}
+          <div className="flex">
+            <div className="  w-[calc(100%/4)] px-7 py-3 border-r">
+              <span hidden>Domestic Warranty</span>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r ">
+              <button className="py-2 px-5 bg-colorMain text-white">
+                Add To Cart{" "}
+              </button>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  border-r ">
+              <button className="py-2 px-5 bg-colorMain text-white">
+                Add To Cart{" "}
+              </button>
+            </div>
+            <div className="  w-[calc(100%/4)] px-7 py-3  ">
+              <span></span>
+            </div>
           </div>
         </div>
       </div>
