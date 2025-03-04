cabb8598c51a6823b9a12eac4512ac9d6af12b5d
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2025-03-04 14:22:11 +0700

    FE page home update sliceProduct

--- frontend_ecommerce_interior/src/components/Home/SliceProduct/SliceProduct.jsx
+++ frontend_ecommerce_interior/src/components/Home/SliceProduct/SliceProduct.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+import { GoArrowRight } from "react-icons/go";
 
 const SliceProduct = () => {
   return (
@@ -22,7 +24,28 @@ const SliceProduct = () => {
           </button>
         </div>
       </div>
-      <div className="w-[calc(100%/3)] bg-red-300">kkk</div>
+      <div className="w-[calc(100%/3)]   p-7">
+        <div className="w-full h-full relative">
+          <img
+            src="https://images.pexels.com/photos/1034584/pexels-photo-1034584.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+          <div className=" absolute bottom-5 left-5 w-[100%] h-[120px] flex items-end">
+            <div className="bg-white/70 w-[55%] h-full p-7 flex flex-col justify-center gap-3">
+              <span>01 --- Bed Room</span>
+              <p className="font-poppins font-semibold text-2xl">Inner Peace</p>
+            </div>
+
+            <div>
+              {" "}
+              <button className="px-3 py-3 bg-colorMain text-white">
+                <GoArrowRight />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="w-[calc(100%/3)] bg-green-400">kkk</div>
     </div>
   );
