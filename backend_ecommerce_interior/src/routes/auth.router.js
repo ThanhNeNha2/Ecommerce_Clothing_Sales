705c4e213b68433b8f2f7cb7c58d97686e32febc
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2025-03-04 17:39:51 +0700

    4/3/2025

--- backend_ecommerce_interior/src/routes/auth.router.js
+++ backend_ecommerce_interior/src/routes/auth.router.js
@@ -4,6 +4,7 @@ const {
 
   activateAccount,
   resetKeyActivateAccount,
+  reSendKey,
 } = require("../controller/auth.controller");
 
 const router = require("express").Router();
@@ -13,5 +14,6 @@ router.post("/auth/login", Login);
 router.post("/auth/register", Register);
 router.post("/activateAccount/:id", activateAccount);
 router.post("/resetKeyActivateAccount", resetKeyActivateAccount);
+router.post("/reSendKey/:id", reSendKey);
 
 module.exports = router;
--- backend_ecommerce_interior/src/server.js
+++ backend_ecommerce_interior/src/server.js
@@ -2,27 +2,31 @@ import express from "express";
 import mongoose from "mongoose";
 import users from "./routes/users.router";
 import auth from "./routes/auth.router";
-
+var cors = require("cors");
 import bodyParser from "body-parser";
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 8080;
 mongoose
   .connect(process.env.URL_CONNECT_MONGODB)
-  .then(() => {
-    console.log("Connect success!");
-  })
+  // .then(() => {
+  //   console.log("Connect success!");
+  // })
   .catch((error) => {
     console.log("Connect error!", error);
   });
-
+app.get("/test", (req, res) => {
+  res.status(200).json({ message: "API is working!" });
+});
 // Lấy được thông tin truyền lên từ client
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 app.use("/api", users);
 app.use("/api", auth);
 // chuyen thong tin user qua bên nguoi dung chuyen doi nguoi qua bên phuong thuc thu 2 của trinh duyet
 app.listen(port, () =>
   console.log("> Server is up and running on port : " + port)
 );
+// "test": "echo \"Error: no test specified\" && exit 1",
+module.exports = app;
