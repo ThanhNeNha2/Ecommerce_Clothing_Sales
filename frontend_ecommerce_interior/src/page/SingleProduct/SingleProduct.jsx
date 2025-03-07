1a43be9065c5c3483b58bc9c4ef6de299da59bb1
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2025-03-07 16:36:26 +0700

    Link Router

--- frontend_ecommerce_interior/src/page/SingleProduct/SingleProduct.jsx
+++ frontend_ecommerce_interior/src/page/SingleProduct/SingleProduct.jsx
@@ -4,7 +4,6 @@ import DetailProduct from "../../components/SingleProduct/DetailProduct";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import Products from "../../components/Products/Products";
 import Footer from "../../components/Footer/Footer";
-import { Link } from "react-router-dom";
 
 const SingleProduct = () => {
   const [addProduct, setaddProduct] = useState(4);
@@ -18,11 +17,10 @@ const SingleProduct = () => {
           background: "#F9F1E7",
         }}
       >
-        <Link>
-          <div className="flex items-center">
-            <span>Home</span> <MdKeyboardArrowRight />
-          </div>
-        </Link>
+        <div className="flex items-center">
+          <span>Home</span> <MdKeyboardArrowRight />
+        </div>
+
         <div className="flex items-center">
           <span>Shop</span> <MdKeyboardArrowRight />
         </div>
