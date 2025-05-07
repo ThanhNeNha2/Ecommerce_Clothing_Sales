aa21f62e1f35c84a21250cecfcbf1178539abba4
mtriet100505 <ta.coaching.v2@gmail.com>
2025-05-07 14:25:42 +0700

    crud product

--- backend_ecommerce_interior/src/routes/product.router.js
+++ backend_ecommerce_interior/src/routes/product.router.js
@@ -1,1 +1,19 @@
- 
+import express from "express";
+import {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/productController.js";
+
+const router = express.Router();
+
+// Định nghĩa các route
+router.post("/", createProduct);
+router.get("/", getAllProducts);
+router.get("/:id", getProductById);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
+export default router;
